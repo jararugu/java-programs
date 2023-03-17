64b095104d30abee10bdfcf542869b8916c4cb1f
jararugu <113119300+jararugu@users.noreply.github.com>
2023-03-17 16:30:10 +0530

    Add files via upload

---
+++ Person1.java
@@ -0,0 +1,13 @@
+class Person1
+{
+public static void main(String[] args)
+{
+//variable declaration and intilization
+  int age = 21;
+  double hieght = 5.2;
+  System.out.println("person age is:" +age+" years");
+  System.out.println("person hieght is:"  +hieght+" feet");
+  }
+  }
+  
+  
